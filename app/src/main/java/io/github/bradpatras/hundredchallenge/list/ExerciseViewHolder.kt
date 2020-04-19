7fe761184091412cd76f67317a7eb2f3e5f2d837
Brad <BradPatras@users.noreply.github.com>
2020-04-18 21:02:34 -0500

    fixed glitch in progress bar animation, added ripple effect to recyclerview items

--- app/src/main/java/io/github/bradpatras/hundredchallenge/list/ExerciseViewHolder.kt
+++ app/src/main/java/io/github/bradpatras/hundredchallenge/list/ExerciseViewHolder.kt
@@ -1,6 +1,7 @@
 package io.github.bradpatras.hundredchallenge.list
 
 import android.animation.ValueAnimator
+import android.util.Log
 import android.view.View
 import androidx.core.view.doOnLayout
 import androidx.recyclerview.widget.RecyclerView
@@ -19,12 +20,22 @@ class ExerciseViewHolder(val view: View): RecyclerView.ViewHolder(view) {
 
     fun updateProgressBar(progress: Double) {
         view.progress_bar.doOnLayout {
-            val layoutParams = view.progress_bar.layoutParams
-            (progress * view.bg_view.measuredWidth).toInt()
-            val progressWidth = (progress * view.bg_view.measuredWidth).toInt()
-            layoutParams.width = progressWidth
-            view.progress_bar.layoutParams = layoutParams
-            view.progress_bar.visibility = if (progress <= 0) View.INVISIBLE else View.VISIBLE
+
+            // can't compare against zero because of an apparent bug/feature of constraint layout
+            // where the width is getting rounded to zero at some point when the value gets small
+            // enough, causing the view to switch to "match constraints" mode, making the width jump
+            if (progress < 0.01) {
+                view.progress_bar.visibility = View.INVISIBLE
+                Log.i("Bread", "Hiding progress bar. Progress: $progress")
+            } else {
+                Log.i("Bread", "Update progress: $progress")
+                view.progress_bar.visibility = View.VISIBLE
+                val layoutParams = view.progress_bar.layoutParams
+                (progress * view.bg_view.measuredWidth).toInt()
+                val progressWidth = (progress * view.bg_view.measuredWidth).toInt()
+                layoutParams.width = progressWidth
+                view.progress_bar.layoutParams = layoutParams
+            }
         }
     }
 }
